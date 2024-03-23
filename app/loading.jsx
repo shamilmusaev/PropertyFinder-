ec1c53e66189a2dc97514333d0a8cb5fbd049937
Shamil Musaev <dwhitewolf@Shamils-MacBook-Pro.local>
2024-03-23 18:56:44 +0100

    added loader

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,21 @@
+'use client'
 
+import ClipLoader from "react-spinners/ClipLoader"
 
-const Loading = () => {
+const override = {
+    display : 'block',
+    margin: '100px auto',
+}
+const LoadingPage = ({loading}) => {
   return (
-    <div>Loading...</div>
+    <ClipLoader
+    color="#3b82f6"
+    loading={loading}
+    cssOverride={override}
+    size={160}
+    aria-label="Loading Spinner"
+    />
   )
 }
 
-export default Loading
+export default LoadingPage
