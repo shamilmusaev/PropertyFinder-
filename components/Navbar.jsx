0131df51efe0ad73ce8e840dd1fe443e5d165143
Shamil Musaev <dwhitewolf@Shamils-MacBook-Pro.local>
2024-03-26 18:29:57 +0100

    added image avatar from google to our navbar profile buttom

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
-  
+  const profileImage = session?.user?.image
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -28,7 +28,7 @@ const Navbar = () => {
 
     setAuthProviders();
   }, []);
-console.log(session)
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -165,8 +165,10 @@ console.log(session)
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
                       alt=""
+                      width={40}
+                      height={40}
                     />
                   </button>
                 </div>
